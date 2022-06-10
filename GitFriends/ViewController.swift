f060fda79ca91d5c10d50211fb87d457c84f294c
nadiasiddiqah <nadiasiddiqah@gmail.com>
2022-06-09 20:21:16 -0700

    Initial Commit

---
+++ GitFriends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitFriends
+//
+//  Created by Nadia Siddiqah on 6/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
