33b3b40feb62713f3560dd5c19d7d589877f6819
nadiasiddiqah <nadiasiddiqah@gmail.com>
2022-07-03 22:43:48 -0700

    Pass data from Search to Followers screen

--- GitFriends/SceneDelegate.swift
+++ GitFriends/SceneDelegate.swift
@@ -48,6 +48,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         return tabBar
     }
+    
+    // Makes all navigation bar's tintColor green
+    func configureNavigationBar() {
+        UINavigationBar.appearance().tintColor = .systemGreen
+    }
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
